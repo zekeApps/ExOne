8780213a2479f9c88f372a895b3e9637b949b925
zekeApps <sheqel@live.com>
2016-01-28 22:59:17 -0600

    Example resolved

---
+++ ExOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ExOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int numberEntered = Convert.ToInt32(Console.ReadLine());
+            string message = (numberEntered > 0 && numberEntered <= 10) ? ("Valid number") : ("Invalid number");
+            Console.WriteLine(message);
+        }
+    }
+}
